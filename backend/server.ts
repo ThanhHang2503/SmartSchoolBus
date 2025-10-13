028aa873a8ed7e7a23deeb147c10124147d9585d
ThanhHang2503 <thanhhang250305@gmail.com>
2025-10-13 22:00:21 +0700

    edit server

--- backend/server.ts
+++ backend/server.ts
@@ -2,12 +2,13 @@ import bodyParser from "body-parser";
 import express from "express";
 import busRoutes from "./bus/routes/busRoutes";
 import studentRoutes from "./student/routes/studentRoutes";
+import routerRoutes from "./route/routes/routeRoutes";
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use("/buses", busRoutes);
 app.use("/students", studentRoutes);
-
+app.use("/routers", routerRoutes);
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
