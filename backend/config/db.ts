78dc37b6c05d8f1afaeb55b1ec7bab8ed9b81ab8
Truc Vi <trucvidt2903@gmail.com>
2025-11-20 13:58:53 +0700

    up db len ui parent

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -3,6 +3,7 @@ import mysql = require("mysql2/promise");
 export const pool = mysql.createPool({
   host: "localhost",
   user: "root",
+  password: "123456", 
   database: "SSB",
   port: 3306,
 });
